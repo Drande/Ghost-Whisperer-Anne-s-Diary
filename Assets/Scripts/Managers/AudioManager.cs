4a4c940429015db0dea558ed86015e8742972a97
Jorge Fernandez <jorgejgdrj41@gmail.com>
2024-07-23 20:28:45 -0500

    Add volume controls and persistence for audio manager

--- Assets/Scripts/Managers/AudioManager.cs
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -1,12 +1,18 @@
 using System;
 using UnityEngine;
+using UnityEngine.Audio;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour
 {
-    public static AudioManager Instance;
+    public static AudioManager Instance { get; private set; }
     [SerializeField] private Sound[] musicSounds, sfxSounds;
     [SerializeField] private AudioSource musicSource, sfxSource;
+    [SerializeField] private AudioMixer masterMixer;
+    private const string MusicVolumeKey = "MusicVolume";
+    private const string SfxVolumeKey = "SfxVolume";
+    private const string MasterVolumeKey = "MasterVolume";
 
     private void Awake()
     {
@@ -21,28 +27,88 @@ public class AudioManager : MonoBehaviour
         }
     }
 
-    private void HandlePauseChange(bool isPaused)
-    {
-        if (isPaused)
-        {
-            musicSource.Pause();
-        }
-        else
-        {
-            musicSource.UnPause();
-        }
-    }
-
     private void Start()
     {
+        ApplySettings();
         HandleSceneChange(SceneManager.GetActiveScene());
         SceneManager.activeSceneChanged += (previous, current) =>
         {
-            if (previous.name != current.name)
-            {
-                HandleSceneChange(current);
-            }
+            if (previous.name != current.name) HandleSceneChange(current);
         };
+    }
+    
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.M)) {
+            ToggleMute();
+        }
+    }
+
+    public float MusicVolume
+    {
+        get => PlayerPrefs.GetFloat(MusicVolumeKey, 1.0f);
+        set
+        {
+            PlayerPrefs.SetFloat(MusicVolumeKey, value);
+            PlayerPrefs.Save();
+            SetMusicVolume(value);
+        }
+    }
+
+    public float SfxVolume
+    {
+        get => PlayerPrefs.GetFloat(SfxVolumeKey, 1.0f);
+        set
+        {
+            PlayerPrefs.SetFloat(SfxVolumeKey, value);
+            PlayerPrefs.Save();
+            SetSfxVolume(value);
+        }
+    }
+
+    public float MasterVolume
+    {
+        get => PlayerPrefs.GetFloat(MasterVolumeKey, 1.0f);
+        set
+        {
+            PlayerPrefs.SetFloat(MasterVolumeKey, value);
+            PlayerPrefs.Save();
+            SetMasterVolume(value);
+        }
+    }
+
+    public void ToggleMute()
+    {
+        if(masterMixer.GetFloat(MasterVolumeKey, out var volume)) {
+            MasterVolume = volume == 0f ? 0f : 1f;
+        }
+    }
+
+    public void ApplySettings()
+    {
+        // Apply initial settings
+        SetSfxVolume(SfxVolume);
+        SetMusicVolume(MusicVolume);
+        SetMasterVolume(MasterVolume);
+    }
+
+    public void SetMusicVolume(Slider slider) => MusicVolume = slider.value;
+    public void SetSfxVolume(Slider slider) => SfxVolume = slider.value;
+    public void SetMusicVolume(float volume) => SetVolume(MusicVolumeKey, volume);
+    public void SetSfxVolume(float volume) => SetVolume(SfxVolumeKey, volume);
+    public void SetMasterVolume(float volume) => SetVolume(MasterVolumeKey, volume);
+    private void SetVolume(string key, float volume) => masterMixer.SetFloat(key, Mathf.Log10(Mathf.Clamp(volume, 0.0001f, 1.0f)) * 20);
+
+    /// <summary>
+    /// Pause background music, call on Time.TimeScale changes if music should stop and resume during this action.
+    /// </summary>
+    /// <param name="isPaused">Boolean that determines if we are entering pause state.</param>
+    private void HandlePauseChange(bool isPaused)
+    {
+        if (isPaused)
+            musicSource.Pause();
+        else
+            musicSource.UnPause();
     }
 
     private void HandleSceneChange(Scene scene)
@@ -50,7 +116,9 @@ public class AudioManager : MonoBehaviour
         switch (scene.name)
         {
             default:
-                // TODO: Add scene music changes.
+                if(!musicSource.isPlaying) {
+                    PlayMusic("HappyMusic");
+                }
                 break;
         }
     }
