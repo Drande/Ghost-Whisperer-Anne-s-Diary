40cbc79feb88bc57708b83fe79ed9ea0fb4ed562
Jorge Fernandez <jorgejgdrj41@gmail.com>
2024-07-23 22:23:16 -0500

    Add volume controls and update main interface prefabs

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,18 +1,36 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Slider musicVolumeSlider;
+    [SerializeField] private Slider sfxVolumeSlider;
+
+    private void Start()
     {
-        
+        // Initialize slider values with the current settings
+        musicVolumeSlider.value = AudioManager.Instance.MusicVolume;
+        sfxVolumeSlider.value = AudioManager.Instance.SfxVolume;
+
+        // Add listeners to handle value changes
+        musicVolumeSlider.onValueChanged.AddListener(OnMusicVolumeChanged);
+        sfxVolumeSlider.onValueChanged.AddListener(OnSfxVolumeChanged);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMusicVolumeChanged(float value)
     {
-        
+        AudioManager.Instance.MusicVolume = value;
+    }
+
+    private void OnSfxVolumeChanged(float value)
+    {
+        AudioManager.Instance.SfxVolume = value;
+    }
+
+    private void OnDestroy()
+    {
+        // Remove listeners when the object is destroyed to avoid memory leaks
+        musicVolumeSlider.onValueChanged.RemoveListener(OnMusicVolumeChanged);
+        sfxVolumeSlider.onValueChanged.RemoveListener(OnSfxVolumeChanged);
     }
 }
